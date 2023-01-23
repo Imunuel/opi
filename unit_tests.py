f30d02f27ab58cea88a270f40debbe4827cd11cb
Imunuel <prokhorenko_roma@mail.ru>
2023-01-23 19:47:06 +0300

    added pytest

--- unit_tests.py
+++ unit_tests.py
@@ -2,6 +2,7 @@ from laba import read_all_lines
 
 lines = read_all_lines()
 
+
 def total_value_of_operation():
     total = 0
     for line in lines:
@@ -13,12 +14,10 @@ def total_value_of_operation():
             return "Данные введены не корректны"
     return total
 
-def unit_test(value: float):
+
+def test_unit():
+    value = total_value_of_operation()
     assert value != 0, "Суммарное значение операций каким-то образом оказалось равно 0"
     assert value > 0, "Расходы превысили вашу зарплату"
     assert value < 996.74, "Вы не тратите деньги?"
     assert value == 871.07, "Я точно знаю сколько должно остаться на счету..."
-
-if __name__ == "__main__":
-    value = total_value_of_operation()
-    unit_test(value)
